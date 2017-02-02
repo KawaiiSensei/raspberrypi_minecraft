cc64f7644bc56b9c9629fddb5ba60c537014ad79
Complexity <davidzilvys@gmail.com>
2017-02-02 18:20:35 +0000

    Update nuker.py

--- nuker.py
+++ nuker.py
@@ -13,9 +13,9 @@ def nuker():
 		''' Get the player location '''
 		x, y, z = mc.player.getPos()
 		''' Sets the block in a radius of 5 to air '''
-		mc.setblock(x+1, y+1, z+1, x+11, y+11, z+11, air)
+		mc.setBlocks(x-5, y-5, z-5, x+5, y+5, z+5, air)
 		''' Simple wait timer to not overload the program'''
-		sleep(0.1)
+		#sleep(0.1)
 
 ''' Question to begin the program '''
 ask = input("Do you want to run the Nuker Function?\n")
