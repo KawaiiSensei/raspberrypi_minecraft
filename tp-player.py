77ff69de1ad8eedaf26e49e136f5342c075ab840
Complexity <davidzilvys@gmail.com>
2017-02-04 22:11:10 +0000

    Update tp-player.py

--- tp-player.py
+++ tp-player.py
@@ -1,4 +1,4 @@
-'''imports'''
+b'''imports'''
 from mcpi.minecraft import Minecraft
 
 ''' Init '''
@@ -6,35 +6,73 @@ mc = Minecraft.create()
 
 ''' Teleport Functions '''
 
-'''
-In development
 
-def singletp():
-	x, y, z = mc.player.getPos()
-'''
+#In development
+
+def tptocords():
+        x, y, z = mc.player.getPos()
+        a = input("x axis? Left and Right (MAX: -127, 127)| ")
+        b = input("y axis? Height | ")
+        c = input("z axis? Forward and Backwards (MAX: -127, 127)| ")
+        loc = a, b, c
+        print("Teleporting to X:", a, "Y:", b, "Z:", c)
+        mc.player.setPos(a, b, c)
+        mc.postToChat("Teleporting to given location...." )
+
+
+def tptoplayer():
+        print("Teleporting self to player.")
+        ''' Get self location '''
+        x, y, z = mc.player.getPos()
+
+        ''' Get all possible ids of connectec players '''
+        eIds = mc.getPlayerEntityIds()
+
+        ''' Start a for loop to go for as many entities in game '''
+        for entityId in eIds:
+                print(entityId)
+
+def tphere():
+        print("Teleporting" + chosenPlayer + "to you")
 
 def tpall():
-	''' Get self location '''
-	x, y, z = mc.player.getPos()
+        ''' Get self location '''
+        x, y, z = mc.player.getPos()
 
-	''' Get all possible ids of connectec players '''
-	eIds = mc.getPlayerEntityIds()
+        ''' Get all possible ids of connectec players '''
+        eIds = mc.getPlayerEntityIds()
 
-	''' Start a for loop to go for as many entities in game '''
-	for entityId in eIds:
-		''' Set each entites location to be the same as yours '''
-		mc.player.setPos(entityId, x, y, z)
+        ''' Start a for loop to go for as many entities in game '''
+        for entityId in eIds:
+                ''' Set each entites location to be the same as yours '''
+                mc.player.setPos(entityId, x, y, z)
 
 ''' Question to figure out what the user wants to do '''
-ask = input("Do you want to 1: Teleport a single person to your location. 2: Teleport everyone to your location\n")
-''' IF loop checking for the possible outcomes '''
-if ask == 1:
-	''' if chosen 1 go into single tp function '''
-	singletp()
-elif ask == 2:
-	''' if chosen 2 go into tp all function '''
-	tpall()
-else:
-	''' if non entered print exit message '''
-	print("Thank you for using my program")
+while True:
+        ask = raw_input("Do you want to \n1: Teleport to cords. \n2: Teleport to player \n3: Teleport selected player to your location \n4: Teleport everyone to your location \nq: Quit program")
+        ''' IF loop checking for the possible outcomes '''
+        if ask == '1':
+                ''' if chos:en 1 go into single tp function '''
+                tptocords()
+                print("")
+        elif ask == '2':
+                ''' if chosen 2 go into tp all function '''
+                tptoplayer()
+                print("")
+        elif ask == '3':
+                ''' if chosen 3 go into tp all function '''
+                tphere()#
+                print("")
+        elif ask == '4':
+                ''' if chosen 4 go into tp all function '''
+                tpall()
+                print("")
+        else:
+                ''' if non entered print exit message '''
+                print("Thank you for using my program")
+                break
 
+
+
+''' if answer in ['y', 'Y', 'yes', 'Yes', 'YES']:
+    print("this will do the calculation") '''
