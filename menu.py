bcb537e101c2c82c67796b6ed593d78c9c6ce1de
KawaiiSensei <davidzilvys@gmail.com>
2017-02-05 22:52:18 +0000

    Gui system in progress |	Signed-off-by: KawaiiSensei

---
+++ menu.py
@@ -0,0 +1,24 @@
+''' Import all modules '''
+
+''' Start tkinter project '''
+
+
+''' Create a checkbox layout
+
+	[ ] Nuker	(0-100 slider for the radius)
+	[ ] Flying Ship
+'''
+
+''' Create a button to
+	
+	Teleport everyone to yourself
+'''
+
+''' Create two buttons with one list
+
+	~Player List~	Teleport to selected
+					Teleport selected to you
+'''
+
+''' Create 3 entry boxs
+	_X Axis_   _Y Axis_  _Z Axis_   Teleport to
